1a1683c14a429145229df45c9f7c2b4bd7b875e6
MrMernDeveloper <rakibkst16@gmail.com>
2022-10-03 01:55:43 +0600

    Added file

--- src/components/PriceOption/PriceOption.js
+++ src/components/PriceOption/PriceOption.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { CheckCircleIcon } from '@heroicons/react/24/solid'
 
 const PriceOption = ({option}) => {
     return (
         <div className='bg-indigo-300 m-3 rounded-md p-3'>
-            <h3> <span className='text-6xl font-bold text-white'>{option.price}</span>
-                <span className='text-2xl text-gray-200'>/mon</span>
-            </h3>
-            <p>{option.name}</p>
+            <div>
+                <h3> <span className='text-6xl font-bold text-white'>{option.price}</span>
+                    <span className='text-2xl text-gray-200'>/mon</span>
+                </h3>
+                <p className='text-3xl my-4'>{option.name}</p>
+        </div>
+            <div className='flex items-center'>
+                <CheckCircleIcon className='h-6 w-6 text-green-600'></CheckCircleIcon>
+                <p>Awesome Feature </p>
+            </div>
         </div>
     );
 };
